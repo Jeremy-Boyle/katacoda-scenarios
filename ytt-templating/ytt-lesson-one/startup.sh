348ac7a1d0d20052a7dfe5ae66069f8c0d66878b
jeremy.boyle <jeremyboylet@gmail.com>
2021-05-16 12:59:21 -0500

    Wip

--- ytt-templating/ytt-lesson-one/startup.sh
+++ ytt-templating/ytt-lesson-one/startup.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
-wait 2
+cat << EOF > /root/setup.sh
+#!/bin/bash
+
+show_progress()
+{
+  echo -n "Starting"
+  local -r pid="${1}"
+  local -r delay='0.75'
+  local spinstr='\|/-'
+  local temp
+  printf "    \b\b\b\b"
+  echo ""
+  echo "Started"
+  echo -n "Configuring"
+  while true; do
+    sudo grep -i "done" /root/finished &> /dev/null
+    if [[ "$?" -ne 0 ]]; then
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  printf "    \b\b\b\b"
+  echo ""
+  echo "Configured"
+}
+
+show_progress
+EOF
+chmod +x setup.sh
 ./setup.sh
