6d17f51753460136f8d72b137564d903439746a2
jeremy.boyle <jeremyboylet@gmail.com>
2021-05-16 13:32:40 -0500

    Wip

--- ytt-templating/ytt-lesson-one/startup.sh
+++ ytt-templating/ytt-lesson-one/startup.sh
@@ -1,25 +1,4 @@
 #!/bin/bash
-
-show_progress()
-{
-  echo -n "Setting up"
-  local -r pid="${1}"
-  local spinstr='\|/-'
-  local temp
-  while true; do
-    if [[ ! -f "/tmp/finished"]]; then
-      temp="${spinstr#?}"
-      printf " [%c]  " "${spinstr}"
-      spinstr=${temp}${spinstr%"${temp}"}
-      sleep ".75"
-      printf "\b\b\b\b\b\b"
-    else
-      break
-    fi
-  done
-  printf "    \b\b\b\b"
-  echo ""
-  echo "Configured"
-}
-
-show_progress
+echo -n "Starting Please Wait until files show up!\n Once you see the files in the editor you can start!"
+sleep 5
+clear
