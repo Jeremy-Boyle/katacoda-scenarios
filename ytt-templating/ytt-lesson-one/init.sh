d433be84b13033c7ee14f8437395c3529f2c1088
jeremy.boyle <jeremyboylet@gmail.com>
2021-05-16 12:19:25 -0500

    Wip

--- ytt-templating/ytt-lesson-one/init.sh
+++ ytt-templating/ytt-lesson-one/init.sh
@@ -4,4 +4,4 @@
 mkdir -p yaml && \
     cd yaml/ && \
     wget https://github.com/vmware-tanzu/carvel-ytt/releases/download/v0.33.0/ytt-linux-amd64 -o /usr/local/bin/ytt && \
-    chmod +x /usr/local/bin/ytt
+    chmod +x /usr/local/bin/ytt;
