55845bd2d21074ea92441e5ae9c9e77533eb7ec8
jeremy.boyle <jeremyboylet@gmail.com>
2021-05-16 13:23:55 -0500

    Wip

--- ytt-templating/ytt-lesson-one/startup.sh
+++ ytt-templating/ytt-lesson-one/startup.sh
@@ -1,33 +1,3 @@
 #!/bin/bash
-cat << EOF > /root/setup.sh
-#!/bin/bash
-
-show_progress()
-{
-  local -r pid="${1}"
-  local spinstr='\|/-'
-  local temp
-  printf "    \b\b\b\b"
-  echo ""
-  echo -n "Configuring"
-  while true; do
-    if [[ ! -f /root/finshed ]]; then
-      temp="${spinstr#?}"
-      printf " [%c]  " "${spinstr}"
-      spinstr=${temp}${spinstr%"${temp}"}
-      sleep "1"
-      printf "\b\b\b\b\b\b"
-    else
-      break
-    fi
-  done
-  printf "    \b\b\b\b"
-  echo ""
-  echo "Done!"
-}
-
-show_progress
-EOF
-chmod +x setup.sh
-clear
-./setup.sh
+wait 3
+./check.sh
