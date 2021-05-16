dc087913f912698efaf05cfc7826637ccfed2a88
jeremy.boyle <jeremyboylet@gmail.com>
2021-05-16 12:36:40 -0500

    Wip

--- ytt-templating/ytt-lesson-one/install-ytt.sh
+++ ytt-templating/ytt-lesson-one/install-ytt.sh
@@ -3,4 +3,5 @@
 mkdir -p /root/yaml
 wget https://github.com/vmware-tanzu/carvel-ytt/releases/download/v0.33.0/ytt-linux-amd64 -O /usr/local/bin/ytt
 chmod +x /usr/local/bin/ytt
+wait 10;
 echo "done" >> /root/katacoda-finished
